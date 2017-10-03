11a8c6bae0ae52225a8fa68b26ff03b2c1ab0edc
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-10-03 13:58:31 +0200

    Added git username and password passing

--- src/main/groovy/org/springframework/jenkins/cloud/release/SpringCloudReleaseMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/release/SpringCloudReleaseMaker.groovy
@@ -82,7 +82,7 @@ class SpringCloudReleaseMaker implements JdkConfig, TestPublisher,
 				echo "Releasing the project"
 				${setupGitCredentials()}
 				set +x
-				SYSTEM_PROPS="-Dgpg.secretKeyring="\$${gpgSecRing()}" -Dgpg.publicKeyring="\$${gpgPubRing()}" -Dgpg.passphrase="\$${gpgPassphrase()}" -DSONATYPE_USER="\$${sonatypeUser()}" -DSONATYPE_PASSWORD="\$${sonatypePassword()}""
+				SYSTEM_PROPS="-Dreleaser.git.username="\${${githubRepoUserNameEnvVar()}" -Dreleaser.git.password="\${${githubRepoPasswordEnvVar()}" -Dgpg.secretKeyring="\$${gpgSecRing()}" -Dgpg.publicKeyring="\$${gpgPubRing()}" -Dgpg.passphrase="\$${gpgPassphrase()}" -DSONATYPE_USER="\$${sonatypeUser()}" -DSONATYPE_PASSWORD="\$${sonatypePassword()}""
 				java -jar .git/releaser/spring-cloud-release-tools-spring/target/spring-cloud-release-tools-spring-1.0.0.BUILD-SNAPSHOT.jar --releaser.pom.branch=\${$RELEASER_POM_BRANCH_VAR} --spring.config.name=releaser --releaser.maven.system-properties="\${SYSTEM_PROPS}" --full-release --interactive=false || (${cleanGitCredentials()} && exit 1)
 				${cleanGitCredentials()}
 				""")
