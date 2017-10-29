39361bd11d4a2bec946dc87bb3b79f5e2fac2a8b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-10-29 14:45:47 +0100

    Added pipeline base deployment

--- src/main/groovy/org/springframework/jenkins/cloud/ci/SpringCloudPipelinesBaseDeployBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/ci/SpringCloudPipelinesBaseDeployBuildMaker.groovy
@@ -65,67 +65,22 @@ class SpringCloudPipelinesBaseDeployBuildMaker implements JdkConfig, TestPublish
 			}
 			steps {
 				shell("""#!/bin/bash
-				docker build . -t springcloud/pipeline-base
+				echo "Deploying image to DockerHub"
 				docker login -u \$${dockerhubUserNameEnvVar()} -p \$${dockerhubPasswordEnvVar()}
+				echo "Docker images"
+				docker images
+				echo "Building image"
+				docker build . -t springcloud/pipeline-base
+				docker tag springcloud/pipeline-base springcloud/pipeline-base:\${tagName}
 				docker push springcloud/pipeline-base:latest
 				docker push springcloud/pipeline-base:\${tagName}
+				echo "Removing all local images"
+				docker rmi -f springcloud/spring-cloud-pipeline-jenkins
 				""")
 			}
 			configure {
 				SpringCloudNotification.cloudSlack(it as Node)
 			}
-			publishers {
-				archiveJunit gradleJUnitResults()
-			}
 		}
-	}
-
-	private String buildWithDocs() {
-		return """#!/bin/bash -x
-					${setupGitCredentials()}
-					${setOrigin()}
-					${checkoutMaster()}
-					${build()} || exit 1 
-					${syncDocs()} || echo "Failed to sync docs"
-					${cleanGitCredentials()}
-					${dockerBuildAndPush()}
-					"""
-	}
-
-	private String setOrigin() {
-		return "git remote set-url --push origin `git config remote.origin.url | sed -e 's/^git:/https:/'`"
-	}
-
-	private String checkoutMaster() {
-		return "git checkout master && git pull origin master"
-	}
-
-	private String build() {
-		return "./gradlew clean build generateDocs"
-	}
-
-	private String syncDocs() {
-		return """git commit -a -m "Sync docs" && git push origin ${masterBranch()}"""
-	}
-
-	private String buildNumber() {
-		return '${BUILD_NUMBER}'
-	}
-
-	private String dockerBuildAndPush() {
-		return """
-			echo "Deploying image to DockerHub"
-			docker login --username=\$${dockerhubUserNameEnvVar()} --password=\$${dockerhubPasswordEnvVar()}
-			echo "Docker images"
-			docker images
-			echo "Performing Docker Build"
-			docker build -t springcloud/spring-cloud-pipeline-jenkins ./jenkins
-			echo "Docker images post build"
-			docker images
-			echo "Pushing LATEST image to DockerHub"
-			docker push springcloud/spring-cloud-pipeline-jenkins:latest
-			echo "Removing all local images"
-			docker rmi -f springcloud/spring-cloud-pipeline-jenkins
-		"""
 	}
 }
