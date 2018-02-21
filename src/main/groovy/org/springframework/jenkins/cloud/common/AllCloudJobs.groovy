9b4b1e7bc42148ab26e9c1b980cc2c2dc8ab32b6
Ryan Baxter <rbaxter@pivotal.io>
2018-02-20 20:21:25 -0500

    Removing openfeign

--- src/main/groovy/org/springframework/jenkins/cloud/common/AllCloudJobs.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/common/AllCloudJobs.groovy
@@ -23,8 +23,7 @@ class AllCloudJobs {
 	public static final List<String> ALL_JOBS = ['spring-cloud-sleuth', 'spring-cloud-netflix', 'spring-cloud-zookeeper', 'spring-cloud-consul',
 												 'spring-cloud-bus', 'spring-cloud-commons', 'spring-cloud-security', 'spring-cloud-config',
 												 'spring-cloud-cloudfoundry', 'spring-cloud-aws', 'spring-cloud-build', 'spring-cloud-release',
-												 'spring-cloud-cli', 'spring-cloud-contract', 'spring-cloud-vault', 'spring-cloud-gateway',
-						                                                 'spring-cloud-openfeign']
+												 'spring-cloud-cli', 'spring-cloud-contract', 'spring-cloud-vault', 'spring-cloud-gateway']
 	/**
 	 * Some projects need to have the test report generation skipped (since they have no tests).
 	 */
