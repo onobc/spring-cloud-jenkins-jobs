46eb508b54e697235785287f68425d5ed70410c6
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-05-14 13:07:00 +0200

    polish

--- src/main/groovy/org/springframework/jenkins/cloud/compatibility/CompatibilityTasks.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/compatibility/CompatibilityTasks.groovy
@@ -38,7 +38,7 @@ abstract class CompatibilityTasks {
 	}
 
 	protected String compileProductionForBoot() {
-		return """#!/bin/bash
+		return """#!/bin/bash -x
 					set -o errexit
 					${fetchLatestBootVersion()}
 					${bumpBoot()}
@@ -53,13 +53,13 @@ abstract class CompatibilityTasks {
 		echo -e "Getting latest version of Spring Boot"
 		# Uncomment this to get latest version at all (not necessarily 2.0.x)
 		#${SPRING_BOOT_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/boot/spring-boot-starter/maven-metadata.xml | sed -ne '/<latest>/s#\\s*<[^>]*>\\s*##gp')"
-		[[ -z "${SPRING_BOOT_VERSION_VAR}" ]] && ${SPRING_BOOT_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/boot/spring-boot-starter/maven-metadata.xml | grep "<version>${SPRING_BOOT_MINOR}." | tail -1 | sed -ne '/<version>/s#\\s*<[^>]*>\\s*##gp')"
+		[[ -z "\$${SPRING_BOOT_VERSION_VAR}" ]] && ${SPRING_BOOT_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/boot/spring-boot-starter/maven-metadata.xml | grep "<version>${SPRING_BOOT_MINOR}." | tail -1 | sed -ne '/<version>/s#\\s*<[^>]*>\\s*##gp')"
 		echo -e "Latest version of boot is [\$${SPRING_BOOT_VERSION_VAR}]"
 """
 	}
 
 	protected String runTestsForBoot() {
-		return """#!/bin/bash
+		return """#!/bin/bash -x
 					set -o errexit
 					${fetchLatestBootVersion()}
 					${bumpBoot()}
