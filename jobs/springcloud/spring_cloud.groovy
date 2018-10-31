3575dfdf51ef90b1aba689a9e49f7225a5a229b5
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-10-31 09:19:09 +0100

    Alibaba

--- jobs/springcloud/spring_cloud.groovy
+++ jobs/springcloud/spring_cloud.groovy
@@ -124,7 +124,8 @@ new SpringCloudDeployBuildMaker(dsl, "spring-cloud-incubator").with {
 	deploy("spring-cloud-alibaba", "1.x")
 	deploy("spring-cloud-contract-raml")
 }
-new SpringCloudReleaseMaker(dsl, "spring-cloud-incubator").release("spring-cloud-alibaba")
+new SpringCloudReleaseMaker(dsl, "spring-cloud-incubator")
+		.release("spring-cloud-alibaba")
 
 // SLEUTH
 new SleuthBenchmarksBuildMaker(dsl).buildSleuth()
