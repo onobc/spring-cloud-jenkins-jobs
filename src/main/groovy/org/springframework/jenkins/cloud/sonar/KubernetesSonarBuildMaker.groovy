a9c17b468e85b3f77b813b092c18bf4336d38fe2
Ryan Baxter <rbaxter@pivotal.io>
2019-08-21 12:35:09 -0400

    Customizing kubernetes sonar build

--- src/main/groovy/org/springframework/jenkins/cloud/sonar/KubernetesSonarBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/sonar/KubernetesSonarBuildMaker.groovy
@@ -17,9 +17,16 @@ class KubernetesSonarBuildMaker extends SonarBuildMaker {
 	@Override
 	Closure defaultSteps() {
 		return buildStep {
-
-		} << super.defaultSteps() <<  buildStep {
-
+			environmentVariables {
+				env("_SERVICE_OCCURENCE", "5")
+			}
+			shell('./mvnw clean -Dservice.occurence=${_SERVICE_OCCURENCE} #org.jacoco:jacoco-maven-plugin:prepare-agent install -Psonar -U')
+			shell("""\
+				echo "Running sonar please wait..."
+				set +x
+				./mvnw \$SONAR_MAVEN_GOAL -Psonar -Dsonar.host.url=\$SONAR_HOST_URL -Dsonar.login=\$SONAR_AUTH_TOKEN || ${postAction()}
+				set -x
+				""")
 		}
 	}
 }
