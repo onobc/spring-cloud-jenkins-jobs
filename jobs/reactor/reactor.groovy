7fc65feaa7e4d397adad182c92393bfd4bfdcadd
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-01-09 09:45:20 +0100

    OPtimize imports

--- jobs/reactor/reactor.groovy
+++ jobs/reactor/reactor.groovy
@@ -2,43 +2,10 @@ package reactor
 
 import javaposse.jobdsl.dsl.DslFactory
 
-import org.springframework.jenkins.cloud.ci.ConsulSpringCloudDeployBuildMaker
-import org.springframework.jenkins.cloud.ci.CustomJobFactory
-import org.springframework.jenkins.cloud.ci.SleuthBenchmarksBuildMaker
-import org.springframework.jenkins.cloud.ci.SpringCloudDeployBuildMaker
-import org.springframework.jenkins.cloud.ci.SpringCloudDeployBuildMakerBuilder
-import org.springframework.jenkins.cloud.ci.SpringCloudKubernetesDeployBuildMaker
-import org.springframework.jenkins.cloud.ci.SpringCloudReleaseToolsBuildMaker
-import org.springframework.jenkins.cloud.ci.VaultSpringCloudDeployBuildMaker
-import org.springframework.jenkins.cloud.common.CloudJdkConfig
-import org.springframework.jenkins.cloud.compatibility.ManualBootCompatibilityBuildMaker
-import org.springframework.jenkins.cloud.e2e.BreweryEndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.CloudFoundryBreweryTestExecutor
-import org.springframework.jenkins.cloud.e2e.CloudFoundryEndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.EndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.Jdk11BreweryEndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.NetflixEndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.SleuthEndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.SpringCloudSamplesEndToEndBuildMaker
-import org.springframework.jenkins.cloud.e2e.SpringCloudSamplesEndToEndBuilder
-import org.springframework.jenkins.cloud.e2e.SpringCloudSamplesTestsBuildMaker
 import org.springframework.jenkins.cloud.release.SpringCloudMetaReleaseMaker
 import org.springframework.jenkins.cloud.release.SpringCloudMetaReleaseRepoPurger
 import org.springframework.jenkins.cloud.release.SpringCloudReleaseMaker
-import org.springframework.jenkins.cloud.release.SpringCloudReleaseMasterMaker
 import org.springframework.jenkins.cloud.release.SpringCloudReleaserOptions
-import org.springframework.jenkins.cloud.sonar.ConsulSonarBuildMaker
-import org.springframework.jenkins.cloud.sonar.KubernetesSonarBuildMaker
-import org.springframework.jenkins.cloud.sonar.SonarBuildMaker
-import org.springframework.jenkins.common.job.BuildAndDeploy
-
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.ALL_DEFAULT_JOBS
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.ALL_JOBS_WITH_TESTS
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.ALL_RELEASER_JOBS
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.ALL_STREAM_JOBS_FOR_RELEASER
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.CUSTOM_BUILD_JOBS
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.JOBS_WITHOUT_TESTS
-import static org.springframework.jenkins.cloud.common.AllCloudJobs.JOBS_WITH_BRANCHES
 
 DslFactory dsl = this
 
