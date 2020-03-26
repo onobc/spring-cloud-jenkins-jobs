b12416e3918c07d2069b931e0a5a5ea105b0ff9f
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-03-26 14:55:45 +0100

    Adds missing entries for the pittest plugin

--- src/main/groovy/org/springframework/jenkins/cloud/common/PitestPublisher.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/common/PitestPublisher.groovy
@@ -8,16 +8,35 @@ class PitestPublisher {
 	private final Node rootNode
 	private final def propertiesNode
 	private final def tap
+	private final def buildConditions
 
 	PitestPublisher(Node rootNode) {
 		this.rootNode = rootNode
 		this.propertiesNode = rootNode / 'publishers'
 		this.tap = propertiesNode / 'org.jenkinsci.plugins.pitmutation.PitPublisher'
+		this.buildConditions = this.tap / 'buildConditions__'
 		killRatioMustImprove()
+		minimumKillRatio()
+		mustImproveCondition()
+		percentageThresholdCondition()
 	}
 
 	void killRatioMustImprove(boolean value = true) {
 		(tap / 'killRatioMustImprove__').setValue(value)
+	}
+
+	void minimumKillRatio(double value = 0.0) {
+		(tap / 'minimumKillRatio__').setValue(value)
+	}
+
+	void mustImproveCondition() {
+		(buildConditions / 'org.jenkinsci.plugins.pitmutation.PitPublisher_-MustImproveCondition' / 'outer-class')
+	}
+
+	void percentageThresholdCondition() {
+		def condition = (buildConditions / 'org.jenkinsci.plugins.pitmutation.PitPublisher_-PercentageThresholdCondition')
+		(condition / 'outer-class')
+		(condition / 'percentage').setValue(0.0)
 	}
 
 	static PitestPublisher cloudMutation(Node rootNode, @DelegatesTo(PitestPublisher) Closure closure = Closure.IDENTITY) {
