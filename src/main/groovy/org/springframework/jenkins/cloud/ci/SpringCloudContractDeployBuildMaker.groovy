cd15172e9bffb296417f3b58f45e77f004000e3a
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-08-04 19:20:28 +0200

    Updated jdk8 version for sc-contract

--- src/main/groovy/org/springframework/jenkins/cloud/ci/SpringCloudContractDeployBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/ci/SpringCloudContractDeployBuildMaker.groovy
@@ -18,7 +18,7 @@ import org.springframework.jenkins.common.job.TestPublisher
 class SpringCloudContractDeployBuildMaker implements JdkConfig, TestPublisher, CloudCron,
 		SpringCloudJobs, Maven, CustomJob {
 	private static final Map<String, String> JDKS = [
-			"jdk8" : "8.0.262.hs-adpt",
+			"jdk8" : "8.0.265.hs-adpt",
 			"jdk11" : "11.0.8.hs-adpt",
 			"openjdk11" : "11.0.8.hs-adpt",
 			"openjdk14" : "14.0.2.hs-adpt",
