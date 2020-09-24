761187568dfd363e7b6f351facc3d0e357a8e692
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-09-24 10:49:57 +0200

    Fixes missing export

--- src/main/groovy/org/springframework/jenkins/cloud/common/SpringCloudJobs.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/common/SpringCloudJobs.groovy
@@ -82,7 +82,7 @@ if [ -n "\$(type gtimeout)" ]; then gtimeout 10s docker ps -a -q | xargs -n 1 -P
 		echo -e "Getting latest version of Spring Cloud"
 		# Uncomment this to get latest version at all (not necessarily for the minor)
 		#${CURRENT_CLOUD_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/cloud/spring-cloud-starter-build/maven-metadata.xml | sed -ne '/<latest>/s#\\s*<[^>]*>\\s*##gp')"
-		$CURRENT_CLOUD_VERSION_VAR="\${$CURRENT_CLOUD_VERSION_VAR:-}"
+		export $CURRENT_CLOUD_VERSION_VAR="\${$CURRENT_CLOUD_VERSION_VAR:-}"
 		[[ -z "\$${CURRENT_CLOUD_VERSION_VAR}" ]] && ${CURRENT_CLOUD_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/cloud/spring-cloud-starter-build/maven-metadata.xml | grep "<version>${springCloudMinor}." | grep "SNAPSHOT" | tail -1 | sed -ne '/<version>/s#\\s*<[^>]*>\\s*##gp')"
 		echo -e "Latest version of cloud for minor [${springCloudMinor}] is [\$${CURRENT_CLOUD_VERSION_VAR}]"
 """
@@ -93,7 +93,7 @@ if [ -n "\$(type gtimeout)" ]; then gtimeout 10s docker ps -a -q | xargs -n 1 -P
 		echo -e "Getting latest version of Spring Cloud"
 		# Uncomment this to get latest version at all (not necessarily for the minor)
 		#${CURRENT_BOOT_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/boot/spring-boot-starter/maven-metadata.xml | sed -ne '/<latest>/s#\\s*<[^>]*>\\s*##gp')"
-		$CURRENT_BOOT_VERSION_VAR="\${$CURRENT_BOOT_VERSION_VAR:-}"
+		export $CURRENT_BOOT_VERSION_VAR="\${$CURRENT_BOOT_VERSION_VAR:-}"
 		[[ -z "\$${CURRENT_BOOT_VERSION_VAR}" ]] && ${CURRENT_BOOT_VERSION_VAR}="\$( curl https://repo.spring.io/libs-snapshot-local/org/springframework/boot/spring-boot-starter/maven-metadata.xml | grep "<version>${bootMinor}." | grep "SNAPSHOT" | tail -1 | sed -ne '/<version>/s#\\s*<[^>]*>\\s*##gp')"
 		echo -e "Latest version of boot minor [${bootMinor}] is [\$${CURRENT_BOOT_VERSION_VAR}]"
 """
