8bdaa8c048b6a6768efd1b7eeef368a138bed820
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-11-10 14:35:28 +0100

    Fixed tests

--- src/test/groovy/org/springframework/jenkins/cloud/common/AllCloudJobsSpec.groovy
+++ src/test/groovy/org/springframework/jenkins/cloud/common/AllCloudJobsSpec.groovy
@@ -10,10 +10,10 @@ class AllCloudJobsSpec extends Specification {
 
 		where:
 			cloud               || boot
-			null                || "2.4"
-			"null"              || "2.4"
-			"2020.0.0"          || "2.4"
-			"2020.0.0-SNAPSHOT" || "2.4"
+			null                || "2.4.0-RC1"
+			"null"              || "2.4.0-RC1"
+			"2020.0.0"          || "2.4.0-RC1"
+			"2020.0.0-SNAPSHOT" || "2.4.0-RC1"
 			"Hoxton.RELEASE"    || "2.3"
 			"Greenwich.SR1"     || "2.1"
 	}
