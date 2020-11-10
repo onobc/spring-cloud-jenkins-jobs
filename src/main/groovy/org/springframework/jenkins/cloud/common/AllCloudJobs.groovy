53de5954a6e100a13b42271b8a20b958e6b4332f
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-11-10 13:14:33 +0100

    Can't use snapshots of 2.4 boot

--- src/main/groovy/org/springframework/jenkins/cloud/common/AllCloudJobs.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/common/AllCloudJobs.groovy
@@ -106,8 +106,8 @@ class AllCloudJobs {
 	 */
 	public static final Map<String, String> RELEASE_TRAIN_TO_BOOT_VERSION_MINOR = [
 			// TODO: 2020.0 is the train, 2020.0.0 is like Hoxton.RELEASE not Hoxton
-			"2020.0" : "2.4", // boot version 2.4.x
-			"2020.0.0" : "2.4", // boot version 2.4.x - this should be removed
+			"2020.0" : "2.4.0-RC1", // boot version 2.4.x
+			"2020.0.0" : "2.4.0-RC1", // boot version 2.4.x - this should be removed
 			"hoxton" : "2.3", // boot version 2.2.x
 			"greenwich" : "2.1" // boot version 2.1.x
 	]
