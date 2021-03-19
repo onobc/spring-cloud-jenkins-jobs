06d8837c7841b6f444e69db3cf985581d7c260bc
spencergibb <sgibb@vmware.com>
2021-03-19 11:37:44 -0400

    Revert "removes jdk 15"
    This reverts commit 66da02a6

--- jobs/springcloud/spring_cloud_ci.groovy
+++ jobs/springcloud/spring_cloud_ci.groovy
@@ -33,6 +33,10 @@ new SpringCloudDeployBuildMaker(dsl).with { SpringCloudDeployBuildMaker maker ->
 				.prefix("spring-cloud-${jdk11()}").jdkVersion(jdk11())
 				.upload(false).build().deploy(it)
 		new SpringCloudDeployBuildMakerBuilder(dsl)
+				.prefix("spring-cloud-${jdk15()}").jdkVersion(jdk15())
+				.onGithubPush(false).cron(oncePerDay())
+				.upload(false).build().deploy(it)
+		new SpringCloudDeployBuildMakerBuilder(dsl)
 				.prefix("spring-cloud-${jdk16()}").jdkVersion(jdk16())
 				.onGithubPush(false).cron(oncePerDay())
 				.upload(false).build().deploy(it)
@@ -44,6 +48,9 @@ new SpringCloudDeployBuildMaker(dsl).with { SpringCloudDeployBuildMaker maker ->
 		// JDK compatibility
 		new SpringCloudDeployBuildMakerBuilder(dsl)
 				.prefix("spring-cloud-${jdk11()}").jdkVersion(jdk11())
+				.upload(false).build().deployWithoutTests(it)
+		new SpringCloudDeployBuildMakerBuilder(dsl)
+				.prefix("spring-cloud-${jdk15()}").jdkVersion(jdk15()).onGithubPush(false).cron(oncePerDay())
 				.upload(false).build().deployWithoutTests(it)
 		new SpringCloudDeployBuildMakerBuilder(dsl)
 				.prefix("spring-cloud-${jdk16()}").jdkVersion(jdk16()).onGithubPush(false).cron(oncePerDay())
@@ -59,6 +66,7 @@ CUSTOM_BUILD_JOBS.each { String projectName ->
 	new CloudJdkConfig().with {
 		new CustomJobFactory(dsl).deploy(projectName)
 		new CustomJobFactory(dsl).jdkVersion(projectName, jdk11())
+		new CustomJobFactory(dsl).jdkVersion(projectName, jdk15())
 		new CustomJobFactory(dsl).jdkVersion(projectName, jdk16())
 	}
 	List<String> branches = JOBS_WITH_BRANCHES[projectName]
