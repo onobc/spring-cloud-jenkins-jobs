4c8f175fadb830c06b167354176e551a7d0595c3
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-05-26 09:10:23 +0200

    Disables hoxton wavefront

--- src/main/groovy/org/springframework/jenkins/cloud/e2e/BreweryEndToEndBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/e2e/BreweryEndToEndBuildMaker.groovy
@@ -29,7 +29,9 @@ class BreweryEndToEndBuildMaker extends EndToEndBuildMaker {
 		super.build("$prefix-eureka", repoName(), "runAcceptanceTests.sh -t EUREKA $defaultSwitches", oncePerDay())
 		super.build("$prefix-consul", repoName(), "runAcceptanceTests.sh -t CONSUL $defaultSwitches", oncePerDay())
 		super.build("$prefix-zookeeper", repoName(), "runAcceptanceTests.sh -t ZOOKEEPER $defaultSwitches", oncePerDay())
-		super.build("$prefix-wavefront", repoName(), "runAcceptanceTests.sh -t WAVEFRONT $defaultSwitches", oncePerDay())
+		if (!prefix.toLowerCase().startsWith("hoxton")) {
+			super.build("$prefix-wavefront", repoName(), "runAcceptanceTests.sh -t WAVEFRONT $defaultSwitches", oncePerDay())
+		}
 	}
 
 	protected String repoName() {
