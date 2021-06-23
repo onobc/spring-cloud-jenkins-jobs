0ae522e81e3de392be24f2b78861f933146ef19f
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-06-23 15:53:46 +0200

    Added observability seed

---
+++ seed/projects/observability_seed.groovy
@@ -0,0 +1,26 @@
+job('observability-seed') {
+    triggers {
+        githubPush()
+    }
+    jdk("jdk8")
+    scm {
+        git {
+            remote {
+                github('spring-cloud/spring-cloud-jenkins-jobs')
+            }
+            branch("main")
+        }
+    }
+    steps {
+        gradle("clean build")
+        dsl {
+            external('jobs/springobservability/*.groovy')
+            removeAction('DISABLE')
+            removeViewAction('DELETE')
+            ignoreExisting(false)
+            additionalClasspath([
+                    'src/main/groovy', 'src/main/resources', 'build/lib/*.jar'
+            ].join("\n"))
+        }
+    }
+}
