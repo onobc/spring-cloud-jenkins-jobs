5265e5e14b4330ae593456b835386bc516ae6aac
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-07-15 11:11:04 +0200

    Fix

--- src/test/groovy/org/springframework/jenkins/cloud/e2e/BreweryEndToEndBuildMakerSpec.groovy
+++ src/test/groovy/org/springframework/jenkins/cloud/e2e/BreweryEndToEndBuildMakerSpec.groovy
@@ -4,15 +4,6 @@ import spock.lang.Specification
 
 class BreweryEndToEndBuildMakerSpec extends Specification {
 
-	def 'should not use boot function for a release train that uses a concrete boot version'() {
-		given:
-			BreweryEndToEndBuildMaker maker = new BreweryEndToEndBuildMaker()
-		when:
-			String switches = maker.defaultSwitches("2020.0")
-		then:
-			!switches.contains("bootVersion")
-	}
-
 	def 'should use boot function for a release train that does not use a concrete boot version'() {
 		given:
 			BreweryEndToEndBuildMaker maker = new BreweryEndToEndBuildMaker()
