04125bdeee5f75b04c8e7a94207c9bf849ec23f1
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-07-15 11:06:35 +0200

    Fixed tests

--- src/test/groovy/org/springframework/jenkins/cloud/common/AllCloudJobsSpec.groovy
+++ src/test/groovy/org/springframework/jenkins/cloud/common/AllCloudJobsSpec.groovy
@@ -1,19 +1,21 @@
 package org.springframework.jenkins.cloud.common
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class AllCloudJobsSpec extends Specification {
 
+	@Unroll
 	def 'get correct boot version [#boot] for cloud release train [#cloud]'() {
 		expect:
 			AllCloudJobs.bootForReleaseTrain(cloud).startsWith(boot)
 
 		where:
 			cloud               || boot
-			null                || "2.4"
-			"null"              || "2.4"
-			"2020.0.0"          || "2.4"
-			"2020.0.0-SNAPSHOT" || "2.4"
+			null                || "2.5"
+			"null"              || "2.5"
+			"2020.0.0"          || "2.5"
+			"2020.0.0-SNAPSHOT" || "2.5"
 			"Hoxton.RELEASE"    || "2.3"
 			"Greenwich.SR1"     || "2.1"
 	}
