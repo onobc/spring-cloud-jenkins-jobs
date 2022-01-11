1ca8a5db9d6df69b79336ff9c30190055045f4b1
spencergibb <sgibb@pivotal.io>
2022-01-11 16:56:45 -0500

    debugging

--- src/main/groovy/org/springframework/jenkins/cloud/compatibility/VaultCompatibilityBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/compatibility/VaultCompatibilityBuildMaker.groovy
@@ -83,6 +83,7 @@ class VaultCompatibilityBuildMaker extends CompatibilityBuildMaker implements Ha
 	}
 
 	protected String antiPermgenAndJava7TlsHack(branchName) {
+		println "branchName = ${branchName}"
 		if (branchName == "main") {
 			return '#!/bin/bash -x\nexport MAVEN_OPTS="-Xms256M -Xmx1024M -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=4096M -Dhttps.protocols=TLSv1.2"'
 		}
