179a94a8db2fdd21b98ca74b199ca96a1cdf44ee
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-04-20 13:22:50 +0200

    Build 2021.0.x branch for contract samples

--- src/main/groovy/org/springframework/jenkins/cloud/e2e/SpringCloudContractSamplesEndToEndBuilder.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/e2e/SpringCloudContractSamplesEndToEndBuilder.groovy
@@ -30,5 +30,30 @@ class SpringCloudContractSamplesEndToEndBuilder extends SpringCloudSamplesEndToE
                     .withMavenTests(false)
                     .withGradleTests(true)
         }.build(dsl)
+
+        builderBranchName = "2021.0.x"
+        new SpringCloudSamplesEndToEndBuilder().with {
+            it.withRepoName("spring-cloud-contract-samples")
+                    .withProjectName("spring-cloud-contract-samples-build-maven-only")
+                    .withBranchName(builderBranchName)
+                    .withEnvs(["SKIP_COMPATIBILITY": "true", "SKIP_DOCS" : "true"])
+                    .withCronExpr(oncePerDay())
+                    .withJdk(jdkVersion)
+                    .withScriptName("scripts/runMavenBuilds.sh")
+                    .withMavenTests(true)
+                    .withGradleTests(false)
+        }.build(dsl)
+
+        new SpringCloudSamplesEndToEndBuilder().with {
+            it.withRepoName("spring-cloud-contract-samples")
+                    .withProjectName("spring-cloud-contract-samples-build-gradle-only")
+                    .withBranchName(builderBranchName)
+                    .withEnvs(["SKIP_COMPATIBILITY": "true", "SKIP_DOCS" : "true"])
+                    .withCronExpr(oncePerDay())
+                    .withJdk(jdkVersion)
+                    .withScriptName("scripts/runGradleBuilds.sh")
+                    .withMavenTests(false)
+                    .withGradleTests(true)
+        }.build(dsl)
     }
 }
